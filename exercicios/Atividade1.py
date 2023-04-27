dd7656242bb807796ba26f09e4792d2c1c95a473
ViniciusRossetto <128000399+ViniciusRossetto@users.noreply.github.com>
2023-04-26 22:10:19 -0300

    delete_error

--- exercicios/Atividade1.py
+++
@@ -1,7 +0,0 @@
-#Crie um algoritmo que receba um valor em metros e converta para centímetros.
-
-from pack.contas import metros_para_centimetros
-
-valor=float(input('Digite a metragem: '))
-resultado=metros_para_centimetros(valor)
-print(resultado)
